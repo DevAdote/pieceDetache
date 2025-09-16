2b710d81ed2579e481f9f3c452830a2d82250bf7
DevAdote <elieabeladote@gmail.com>
2025-09-16 11:50:15 +0200

    Affichage de tout les articles de pieces automobiles

---
+++ script/pieces.js
@@ -0,0 +1,49 @@
+// Récupération des pièces depuis le fichier JSON
+    const reponse = await fetch("../pieces-autos.json");
+    const pieces = await reponse.json();
+
+    const article = pieces[0];
+    
+
+ for (let i=0; i < pieces.length; i++){
+
+        //Creation d'un article dedier a une piece automobile
+        const pieceElement = document.createElement("article");
+
+        const imageElement = document.createElement("img");
+	    imageElement.src = pieces[i].image;
+
+	    const nomElement = document.createElement("h2");
+	    nomElement.innerText = article.nom;
+
+        const prixElement = document.createElement("p");
+        // prixElement.innerText = `Prix: ${article.prix} €`;
+        prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;   //L’opérateur ternaire s’utilise lorsque l’on doit choisir entre deux possibilité
+                                                                                             //La syntaxe générale de l’opérateur ternaire est formulée ainsi : expression à tester ? valeur si vrai : valeur si faux.
+        document.body.appendChild(prixElement);
+        const categorieElement = document.createElement("p");  
+        // categorieElement.innerText = article.categorie;
+        categorieElement.innerText = article.categorie ?? "(aucune catégorie)"; //l’opérateur nullish secrit avec  deux ?? sous la forme suivante :
+                                                                                //expression à tester ?? valeur de substitution, 
+                                                                            // il prend la valeur de substitution lorsque la valeur est null, et donc qu’elle n’a pas de valeur, ou bien lorsqu’elle est undefined,   
+
+
+        const descriptionElement = document.createElement("p");
+        descriptionElement.innerText = article.description ?? "(Pas de description pour le moment.)";
+
+        const disponElement = document.createElement("p");
+        disponElement.innerHTML = `Disponibilite: ${article.disponibilite = true ? "En stock" : "Rupture de stock"}`
+
+
+        //Affichage des elements dans le DOM
+
+        const sectionFiches = document.querySelector(".fiches");
+        sectionFiches.appendChild(pieceElement);
+        pieceElement.appendChild(imageElement);
+        pieceElement.appendChild(nomElement);
+        pieceElement.appendChild(prixElement);
+        pieceElement.appendChild(categorieElement);
+        pieceElement.appendChild(descriptionElement);
+        pieceElement.appendChild(disponElement);
+ }
+	
