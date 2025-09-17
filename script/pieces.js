ac536b917c1b1e71237b8f2e4795cdd9f4d1990c
DevAdote <elieabeladote@gmail.com>
2025-09-17 17:34:56 +0200

    Fin de la partie 2

--- script/pieces.js
+++ script/pieces.js
@@ -1,149 +1,151 @@
 // Récupération des pièces depuis le fichier JSON
-    const reponse = await fetch("../pieces-autos.json");
-    const pieces = await reponse.json();
+const reponse = await fetch('../pieces-autos.json');
+const pieces = await reponse.json();
 
-    const article = pieces[0];
-    
+function genererPieces(pieces){
+    for (let i = 0; i < pieces.length; i++) {
 
- for (let i=0; i < pieces.length; i++){
-
-        //Creation d'un article dedier a une piece automobile
-        const pieceElement = document.createElement("article");
-
-        const imageElement = document.createElement("img");
-	    imageElement.src = pieces[i].image;
-
-	    const nomElement = document.createElement("h2");
-	    nomElement.innerText = article.nom;
-
-        const prixElement = document.createElement("p");
-        // prixElement.innerText = `Prix: ${article.prix} €`;
-        prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;   //L’opérateur ternaire s’utilise lorsque l’on doit choisir entre deux possibilité
-                                                                                             //La syntaxe générale de l’opérateur ternaire est formulée ainsi : expression à tester ? valeur si vrai : valeur si faux.
-        document.body.appendChild(prixElement);
-        const categorieElement = document.createElement("p");  
-        // categorieElement.innerText = article.categorie;
-        categorieElement.innerText = article.categorie ?? "(aucune catégorie)"; //l’opérateur nullish secrit avec  deux ?? sous la forme suivante :
-                                                                                //expression à tester ?? valeur de substitution, 
-                                                                            // il prend la valeur de substitution lorsque la valeur est null, et donc qu’elle n’a pas de valeur, ou bien lorsqu’elle est undefined,   
-
-
-        const descriptionElement = document.createElement("p");
-        descriptionElement.innerText = article.description ?? "(Pas de description pour le moment.)";
-
-        const disponElement = document.createElement("p");
-        disponElement.innerHTML = `Disponibilite: ${article.disponibilite = true ? "En stock" : "Rupture de stock"}`
-
-
-        //Affichage des elements dans le DOM
-
+        const article = pieces[i];
+        // Récupération de l'élément du DOM qui accueillera les fiches
         const sectionFiches = document.querySelector(".fiches");
+        // Création d’une balise dédiée à une pièce automobile
+        const pieceElement = document.createElement("article");
+        // Création des balises 
+        const imageElement = document.createElement("img");
+        imageElement.src = article.image;
+        const nomElement = document.createElement("h2");
+        nomElement.innerText = article.nom;
+        const prixElement = document.createElement("p");
+        prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
+        const categorieElement = document.createElement("p");
+        categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
+        const descriptionElement = document.createElement("p");
+        descriptionElement.innerText = article.description ?? "Pas de description pour le moment.";
+        const stockElement = document.createElement("p");
+        stockElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock";
+        
+        // On rattache la balise article a la section Fiches
         sectionFiches.appendChild(pieceElement);
+        // On rattache l’image à pieceElement (la balise article)
         pieceElement.appendChild(imageElement);
         pieceElement.appendChild(nomElement);
         pieceElement.appendChild(prixElement);
         pieceElement.appendChild(categorieElement);
+        //Ajout des éléments au DOM pour l'exercice
         pieceElement.appendChild(descriptionElement);
-        pieceElement.appendChild(disponElement);
- }
-	
+        pieceElement.appendChild(stockElement);
+    
+     }
+}
 
-// Gestion des boutons pour le trie croissant
+genererPieces(pieces);
+
+
+
+ //gestion des bouttons pour le trie des prix croissant
 const boutonTrier = document.querySelector(".btn-trier");
 
 boutonTrier.addEventListener("click", function () {
-  const piecesOrdonnees = Array.from(pieces);
-
-  piecesOrdonnees.sort(function (a, b) {
-    return a.prix - b.prix;
-  });
-
-  console.log(piecesOrdonnees);
+    const piecesOrdonnees = Array.from(pieces);
+    piecesOrdonnees.sort(function (a, b) {
+        return a.prix - b.prix;
+     });
+     document.querySelector(".fiches").innerHTML = "";
+    genererPieces(piecesOrdonnees);
 });
 
-// Gestion des boutons pour le filtre
+
 const boutonFiltrer = document.querySelector(".btn-filtrer");
 
 boutonFiltrer.addEventListener("click", function () {
-   const piecesFiltrees = pieces.filter(function (piece) {
-       return piece.prix <= 35;
-   });
-   console.log(piecesFiltrees)
-});
-
-
-
-// Gestion des boutons pour le trie decroissant
-const btnTrieDecroit = document.querySelector(".btn-trierDecroit")
-btnTrieDecroit.addEventListener("click",()=>{
-    const piecesOrdonnes = Array.from(pieces);
-
-    piecesOrdonnes.sort(function (a,b){
-        return b.prix - a.prix;
-    })
-
-    console.log(piecesOrdonnes)
-});
-
-
-// Gestion des boutons pour le filtre sans description
-const boutonFiltrerDecroit = document.querySelector(".btn-filtrerDescript")
-boutonFiltrerDecroit.addEventListener("click",()=>{
-    const pieceFiltre = pieces.filter(function (piece){
-        return piece.description;
+    const piecesFiltrees = pieces.filter(function (piece) {
+        return piece.prix <= 35;
     });
-    console.log(pieceFiltre);
+    document.querySelector(".fiches").innerHTML = "";
+    genererPieces(piecesFiltrees);
 });
 
 
 
-//PIECE ABORDABLE
+ //gestion des bouttons pour le trie des prix decroissant
+const boutonDecroissant = document.querySelector(".btn-decroissant");
+
+boutonDecroissant.addEventListener("click", function () {
+    const piecesOrdonnees = Array.from(pieces);
+    piecesOrdonnees.sort(function (a, b) {
+        return b.prix - a.prix;
+     });
+     document.querySelector(".fiches").innerHTML = "";
+    genererPieces(piecesOrdonnees);
+});
+
+
+
+const boutonNoDescription = document.querySelector(".btn-nodesc");
+
+boutonNoDescription.addEventListener("click", function () {
+    const piecesFiltrees = pieces.filter(function (piece) {
+        return piece.description
+    });
+    document.querySelector(".fiches").innerHTML = "";
+    genererPieces(piecesFiltrees);
+});
+
 
 const noms = pieces.map(piece => piece.nom);
 for(let i = pieces.length -1 ; i >= 0; i--){
-   if(pieces[i].prix > 35){
-       noms.splice(i,1)
-   }
+    if(pieces[i].prix > 35){
+        noms.splice(i,1);
+    }
 }
 console.log(noms)
+//Création de l'en-tête
 
+const pElement = document.createElement('p')
+pElement.innerText = "Pièces abordables";
 //Création de la liste
 const abordablesElements = document.createElement('ul');
 //Ajout de chaque nom à la liste
 for(let i=0; i < noms.length ; i++){
-   const nomElement = document.createElement('li');
-   nomElement.innerText = noms[i];
-   abordablesElements.appendChild(nomElement)
+    const nomElement = document.createElement('li');
+    nomElement.innerText = noms[i];
+    abordablesElements.appendChild(nomElement);
 }
 // Ajout de l'en-tête puis de la liste au bloc résultats filtres
 document.querySelector('.abordables')
-   .appendChild(abordablesElements)
+    .appendChild(pElement)
+    .appendChild(abordablesElements);
 
 
-// PIECE DISPONIBLE
 
-const nomDisponible = pieces.map(piece=>piece.nom);
-const PrixDisponible = pieces.map(piece=>piece.prix)
+//Code Exercice 
+const nomsDisponibles = pieces.map(piece => piece.nom)
+const prixDisponibles = pieces.map(piece => piece.prix)
 
-for (let i = pieces.length -1; i>=0; i--)
-{
-    if(pieces[i].disponibilite === false)
-    {
-        nomDisponible.splice(i,1)
-        PrixDisponible.splice(i,1)
+for(let i = pieces.length -1 ; i >= 0; i--){
+    if(pieces[i].disponibilite === false){
+        nomsDisponibles.splice(i,1);
+        prixDisponibles.splice(i,1);
     }
-    // console.log(` ${nomDisponible[i]} - ${PrixDisponible[i]} €`)
-} 
-
-//Creation de la liste
-const disponibleElement = document.createElement('ul');
-
-for(let i=0; i<nomDisponible.length; i++){
-
-    const nomElement = document.createElement('li');
-
-    nomElement.innerHTML =` ${nomDisponible[i]} - ${PrixDisponible[i]} €`;
-
-    disponibleElement.appendChild(nomElement)
 }
-document.querySelector(".disponibles").appendChild(disponibleElement)
+
+const disponiblesElement = document.createElement('ul');
+
+for(let i=0 ; i < nomsDisponibles.length ; i++){
+    const nomElement = document.createElement('li');
+    nomElement.innerText = `${nomsDisponibles[i]} - ${prixDisponibles[i]} €`
+    disponiblesElement.appendChild(nomElement);
+}
+
+const pElementDisponible = document.createElement('p')
+pElementDisponible.innerText = "Pièces disponibles:";
+document.querySelector('.disponibles').appendChild(pElementDisponible).appendChild(disponiblesElement)
+
+const inputPrixMax = document.querySelector('#prix-max')
+inputPrixMax.addEventListener('input', function(){
+    const piecesFiltrees = pieces.filter(function(piece){
+        return piece.prix <= inputPrixMax.value;
+    });
+    document.querySelector(".fiches").innerHTML = "";
+    genererPieces(piecesFiltrees);  
+})
